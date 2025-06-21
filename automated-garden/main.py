675ff683a75bdc1ce8a460f1c6a85a4d1de855f5
Ran9waves <ran9waves@protonmail.com>
2025-06-21 21:42:15 +0100

    delete, update and add args added pending to test

--- automated-garden/main.py
+++ automated-garden/main.py
@@ -1,5 +1,6 @@
 
 import argparse #handles command line arguments
+import sys
 
 import plant_database
 
@@ -12,12 +13,39 @@ def main():
         help="Initializes plant_database.py and prints the actual db contents"
     )
 
-    args = parser.parse_args()
+    parser.add_argument(
+        "--update-plant",
+        action="store_true",
+        help="Updates plant database through user input"
+    )
 
+    parser.add_argument(
+        "--delete-plant",
+        action="store_true",
+        help="Deletes a plant from the database"
+    )
+
+    parser.add_argument(
+        "--add-plant",
+        action="store_true",
+        help="Adds a new plant to the database"
+    )
+
+    args = parser.parse_args()
 
     if args.plant_db:
         import plant_database
+    elif args.update_plant:
+        plant_database.update_plant_info()
+    elif args.delete_plant:
+        plant_database.delete_plant()
+    elif args.add_plant:
+        plant_database.add_plant()
+    else:
+        print("No valid command provided. Use --plant-db to view the database or --update-plant to update it.")
+        sys.exit(1)
 
+    
 #ensures  that main() runs only when you execute * main.py directly (not when imported as a module)
 if __name__ == "__main__":
     main()
